fa7a262c734df25b37bd88d4026c3750b4ca7f4c
Ramiro <rgutierrezv03@educantabria.com>
2023-11-29 21:03:42 +0100

    Inicio del JavaFX

--- ProyectoVAX/src/main/java/com/cafeconpalito/proyectovax/App.java
+++ ProyectoVAX/src/main/java/com/cafeconpalito/proyectovax/App.java
@@ -14,10 +14,11 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+   
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("primary"), 1000, 1000);
         stage.setScene(scene);
         stage.show();
     }
