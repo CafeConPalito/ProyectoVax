72dc3f96add3bbaead606361ef778f564a50766f
Daniel-Espinosa <121551604+Daniel-Espinosa@users.noreply.github.com>
2023-12-01 20:45:28 +0100

    Constructor Controller ;)

---
+++ ProyectoVAX/src/main/java/com/cafeconpalito/proyectovax/GameInfoController.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.cafeconpalito.proyectovax;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author damt207
+ */
+public class GameInfoController implements Initializable {
+
+    @FXML
+    private Label labelGameTitle;
+    @FXML
+    private AnchorPane PanelPadre;
+    @FXML
+    private ImageView imagen;
+    @FXML
+    private ComboBox<String> comboBox;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    
+}
