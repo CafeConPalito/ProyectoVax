8854da2fd7598261efb635210460f27d0fa349c1
Albano Díez de Paulino <111312600+TerciodeMarte@users.noreply.github.com>
2023-12-04 20:59:21 +0100

    Crear Login

--- ProyectoVAX/src/main/java/module-info.java
+++ ProyectoVAX/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.cafeconpalito.proyectovax {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.jfoenix;
+    requires java.base;
     
     opens com.cafeconpalito.controllers to javafx.fxml;
     exports com.cafeconpalito.proyectovax;
