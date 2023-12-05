00d1fca18323752811a0e2fb4f6879cd3714d5e5
Daniel-Espinosa <anareldis@gmail.com>
2023-12-06 00:22:17 +0100

    Responsive terminado A currar chavales!

--- ProyectoVAX/src/main/java/com/cafeconpalito/controllers/PrimaryController.java
+++ ProyectoVAX/src/main/java/com/cafeconpalito/controllers/PrimaryController.java
@@ -6,7 +6,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 
 public class PrimaryController {
@@ -17,6 +16,10 @@ public class PrimaryController {
     private Button allGamesBtn;
     @FXML
     private Pane parent;
+    @FXML
+    private BorderPane layout;
+    @FXML
+    private Button registerButton;
 
     @FXML
     private void switchToSecondary() throws IOException {
@@ -25,10 +28,20 @@ public class PrimaryController {
 
     @FXML
     private void switchToLogIn(ActionEvent event) throws IOException {
-
         
-        parent.getChildren().add(new HBox(App.loadFXML("login")));
+        
+        //Borra todo lo que este dentro del padre
+        //Asi limpiamos la escena
+        //parent.getChildren().clear();
+        
+        layout.setCenter(App.loadFXML("login"));
+        
+        
+    }
 
+    @FXML
+    private void switchToRegister(ActionEvent event) throws IOException {
+        layout.setCenter(App.loadFXML("secondary"));
     }
 
 }
--- ProyectoVAX/src/main/java/com/cafeconpalito/proyectovax/App.java
+++ ProyectoVAX/src/main/java/com/cafeconpalito/proyectovax/App.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import javafx.scene.image.Image;
 
 /**
  * JavaFX App
@@ -18,29 +19,46 @@ public class App extends Application {
     @Override
     public void init() throws Exception {
         //aquí llamo a los métodos que INICIAN LA BASE DE DATOS . se ejecuta antes de start
-        
+
         System.out.println("Hola. Aquí empieza todo");
     }
-   
 
     @Override
     public void start(Stage mainStage) throws IOException {
-        
-        scene = new Scene(loadFXML(   "primary"));
-        
-        
-        mainStage.setResizable(true);
-        //mainStage.setMaxWidth(Double.MAX_VALUE);
-        //mainStage.setMaxHeight(Double.MAX_VALUE);
-        mainStage.setTitle("VaX");
+
+        //Cargando Escena principal para mostrar
+        scene = new Scene(loadFXML("primary"));
         mainStage.setScene(scene);
+        
+        //Tamaño modificable del Stage si o no
+        mainStage.setResizable(true);
+        //Titulo Stage
+        mainStage.setTitle("VaX");
+        //Icono Stage
+        mainStage.getIcons().add(new Image(App.class.getResourceAsStream("images/vax games.png")));
+        double width = scene.getWidth();
+        double height = scene.getHeight();
+        
+        //Mostrando Escena principal
         mainStage.show();
+
+        //Tamaño minimo Stage
+        mainStage.setMinWidth(mainStage.getWidth());
+        mainStage.setMinHeight(mainStage.getHeight());
+        
+        
+        //PARA BORRAR
+        //System.out.println(scene.getWidth() + "   " + scene.getHeight());
+        //System.out.println(mainStage.getWidth() + "   " + mainStage.getHeight());
+    
     }
+    
+    
 
     public static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
-    
+
     //Dejar en public para que otros componentes puedan cargar la lista de fxml
     public static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
@@ -50,7 +68,7 @@ public class App extends Application {
     @Override
     public void stop() throws Exception {
         // aquí se ejecuta el código después de cerrar la aplicación. Por ejemplo CERRAR CONEXIÓN BASE DE DATOS
-        
+
         // si quiero salir en otro punto del código y que se ejecute el stop() HAY QUE USAR el :
         // Platform.exit();        
     }
