1d9cbef02594e8ef8501cc3243380d5e373b13a2
Ramiro <112170316+ramirogvalverde@users.noreply.github.com>
2023-12-07 20:11:25 +0100

    register 1 y 2

---
+++ ProyectoVAX/src/main/java/com/cafeconpalito/controllers/Register_1Controller.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.cafeconpalito.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+/**
+ * FXML Controller class
+ *
+ * @author Ramiro
+ */
+public class Register_1Controller implements Initializable {
+
+
+    @FXML
+    private Button allGamesBtn1;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void SelectImage(MouseEvent event) {
+    }
+
+    @FXML
+    private void backBtn(MouseEvent event) {
+    }
+
+    @FXML
+    private void registerBtn(MouseEvent event) {
+    }
+
+    @FXML
+    private void switchToSecondary(ActionEvent event) {
+    }
+
+    @FXML
+    private void imageClicked(MouseEvent event) {
+    }
+
+}
