af4c79195f856696064532e4b1a58612b53fa9ab
Albano Díez de Paulino <adiezd07@educantabria.es>
2023-12-09 13:31:43 +0100

    App build ready

---
+++ ProyectoVAX/src/main/java/com/cafeconpalito/proyectovax/EntryPoint.java
@@ -0,0 +1,13 @@
+package com.cafeconpalito.proyectovax;
+
+/**
+ *
+ * @author Albano Díez de Paulino
+ */
+public class EntryPoint {
+    
+    public static void main(String[] args) {
+        App.main(args);
+    }
+
+}
